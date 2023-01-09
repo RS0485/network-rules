4a9f5f9b938b1c98b1a29712ed3f29411927117e
RS0485 <120764211+RS0485@users.noreply.github.com>
2023-01-09 10:00:05 +0000

    fix ip-geolocation

--- scripts/ip-geolocation.js
+++ scripts/ip-geolocation.js
@@ -27,7 +27,7 @@ $httpClient.get(
           const region = json_data.regionName
           const isp = json_data.isp
 
-          geo_proxy = `本地IP: ${country} ${ip}\n${region}, ${city}, ${isp.substring(0, 12)}`
+          geo_direct = `本地IP: ${country} ${ip}\n${region}, ${city}, ${isp.substring(0, 16)}`
       }
 
       $httpClient.get({
@@ -49,7 +49,7 @@ $httpClient.get(
               const region = json_data.region
               const isp = json_data.isp
 
-              geo_proxy = `代理IP: ${country} ${ip}\n${region}, ${city}, ${isp.substring(0, 12)}`
+              geo_proxy = `代理IP: ${country} ${ip}\n${region}, ${city}, ${isp.substring(0, 16)}`
           }
 
           body = {
