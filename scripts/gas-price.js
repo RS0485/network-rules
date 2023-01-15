2898fbef684d7e359a57661f42c960044f6a655e
RS0485 <120764211+RS0485@users.noreply.github.com>
2023-01-15 13:18:11 +0000

    adjust layout

--- scripts/gas-price.js
+++ scripts/gas-price.js
@@ -57,7 +57,6 @@ $httpClient.get(
             var adjust_date = ''
             var adjust_trend = ''
             var adjust_value = ''
-            var adjust_emoji = '😭'
 
             const reg_adjust_tips = /<div class="tishi"> <span>(.*)<\/span><br\/>([\s\S]+?)<br\/>/
             const adjust_tips_match = data.match(reg_adjust_tips)
@@ -66,8 +65,7 @@ $httpClient.get(
                 adjust_date = adjust_tips_match[1].split('价')[1].slice(0, -2)
 
                 adjust_value = adjust_tips_match[2]
-                adjust_trend = adjust_value.indexOf('下调') > -1 ? '下调' : '上涨'
-                adjust_emoji = adjust_trend == '下调' ? '🤩' : '😭'
+                adjust_trend = adjust_value.indexOf('下调') > -1 ? '↓' : '↑'
 
                 const adjust_value_re = /([\d\.]+)元\/升-([\d\.]+)元\/升/
                 const adjust_value_re2 = /[\d\.]+元\/吨/
@@ -85,7 +83,7 @@ $httpClient.get(
                 }
             }
 
-            const friendly_tips = `${adjust_emoji} ${adjust_date} ${adjust_trend} ${adjust_value}`
+            const friendly_tips = `${adjust_date} ${adjust_trend} ${adjust_value}`
 
             if (prices.length !== 4) {
                 console.log(`解析油价信息失败, 数量=${prices.length}, 请反馈至 @RS0485: URL=${query_addr}`)
